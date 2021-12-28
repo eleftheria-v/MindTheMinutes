8db79319c1b117f8fa66a5a198c98de52a130ac8
DimitrisTsounis <dimitris_tsounis@windowslive.com>
2021-12-28 20:03:49 +0200

    added user model

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Meeting_Minutes.Models
+
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Firstname { get; set; }
+
+        [Required]
+        public string Lastname { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+    }
+}
