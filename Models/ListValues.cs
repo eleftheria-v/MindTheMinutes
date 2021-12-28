8a4ea9346401932bdd32869f26d859c45267d74a
DimitrisTsounis <dimitris_tsounis@windowslive.com>
2021-12-28 22:28:20 +0200

    added ListTypes and ListValues models for risk levels management

---
+++ Models/ListValues.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Meeting_Minutes.Models
+{
+    public class ListValues
+    {
+        [Key]
+        public int ID { get; set; }
+        
+        [Required]
+        public int TypeID { get; set; }
+
+        [Required]
+        public string Value { get; set; }
+
+    }
+}
