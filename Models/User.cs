cd7e91eabfd449997db1f5902d9613b392991a10
DimitrisTsounis <dimitris_tsounis@windowslive.com>
2021-12-30 21:33:13 +0200

    added validation attributes for properties of User model

--- Models/User.cs
+++ Models/User.cs
@@ -8,19 +8,29 @@ namespace Meeting_Minutes.Models
         [Key]
         public int Id { get; set; }
 
-        [Required]
+        [Required (ErrorMessage = "Please enter your first name.")]
+        [Display(Name = "First name")]
+        [StringLength(50)]
         public string Firstname { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter your last name.")]
+        [Display(Name = "Lirst name")]
+        [StringLength(50)]
         public string Lastname { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter a valid username.")]
+        [StringLength(50)]
         public string Username { get; set; }
 
         [Required]
+        [StringLength(50)]
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Please enter a valid phone number.")]
+        [StringLength(25)]
+        [DataType(DataType.PhoneNumber)]
+        [Display(Name = "Phone number")]
         public string PhoneNumber { get; set; }
 
     }
