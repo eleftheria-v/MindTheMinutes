78a52c00b9435e3e56b9ad690210d5003029460d
perse-foni <ppaouri@gmail.com>
2022-01-05 17:12:12 +0200

    Added Meeting Participants Model and Follow Up model

---
+++ Models/FollowupModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Meeting_Minutes.Models
+{
+	public class FollowUp
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public int ItemId { get; set; }
+
+		[Display(Name = "FollowUp Date")]
+		public DateTime Date { get; set; }
+	}
+}
