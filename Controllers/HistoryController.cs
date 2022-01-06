ff2348ebc1f27e492fc5426dd563d11c89f8f6f9
Alexandros Bakalis <alexandros.bakalis@gmail.com>
2022-01-06 17:22:55 +0200

    Create Upcoming,History controllers and views.

---
+++ Controllers/HistoryController.cs
@@ -0,0 +1,21 @@
+﻿using Meeting_Minutes.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Meeting_Minutes.Controllers
+{
+    public class HistoryController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public HistoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var history = _context.Meetings.Where(u => u.MeetingDate<DateTime.Now).ToList();
+            return View(history);
+        }
+
+    }
+}
