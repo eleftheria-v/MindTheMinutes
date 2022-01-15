47cb3bf2517e3aa7037d9a9a4595d965aea41c6a
diokats <dioktys@gmail.com>
2022-01-15 20:59:35 +0200

    Add ShowDateSearchResults method in Meetings History Controller

--- Controllers/HistoryController.cs
+++ Controllers/HistoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Meeting_Minutes.Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Meeting_Minutes.Controllers
 {
@@ -15,6 +16,50 @@ namespace Meeting_Minutes.Controllers
         {
             var history = _context.Meetings.Where(u => u.MeetingDate<DateTime.Now).ToList();
             return View(history);
+        }
+        // POST: Meetings/ShowSearchResults
+        public async Task<IActionResult> ShowSearchResults(string SearchPhrase)
+        {
+            if (String.IsNullOrEmpty(SearchPhrase))
+            {
+                return View("Index", await _context.Meetings.ToListAsync());
+
+            }
+            else
+            {
+                return View("Index", await _context.Meetings.Where(j => j.Title.Contains
+               (SearchPhrase)).ToListAsync());
+            }
+
+        }
+
+
+        public async Task<IActionResult> ShowDateSearchResults(DateTime? dateFrom, DateTime? dateTo)
+
+
+
+        {
+            if (dateFrom.HasValue && dateTo.HasValue)
+            {
+                return View("Index", await _context.Meetings.Where(j => j.MeetingDate >= dateFrom && j.MeetingDate <= dateTo).ToListAsync());
+
+            }
+            else if (dateFrom.HasValue)
+            {
+                return View("Index", await _context.Meetings.Where(j => j.MeetingDate >= dateFrom).ToListAsync());
+
+            }
+            else if (dateTo.HasValue)
+            {
+                return View("Index", await _context.Meetings.Where(j => j.MeetingDate <= dateTo).ToListAsync());
+
+            }
+            else
+            {
+                return View();
+            }
+
+
         }
 
     }
